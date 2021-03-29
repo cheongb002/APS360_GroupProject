687a1c16d2becf5aa6ad76aa99cb1add580f09ad
Brian <cheongb002@gmail.com>
2021-03-28 23:21:26 -0400

    Fixed the paths

--- scripts/train_pretrained.py
+++ scripts/train_pretrained.py
@@ -9,13 +9,13 @@ from utils.train_utils import train_net
 def train_pretrained():
     # set your settings, customizing from the defaults if necessary, see utils/settings.py for all parameters
     run_settings = settings()
-    run_settings.learning_rate = 1e-5 #default is 1e-3
-    run_settings.identifier = "training_template"
-    run_settings.settings_path = "C:\\Users\\kayef\\OneDrive\\Documents\\Year 3\\APS360 - Applied Fundamentals of Machine Learning\\trial_settings"
-    run_settings.features_path = 'C:\\Users\\kayef\\OneDrive\\Documents\\Year 3\APS360 - Applied Fundamentals of Machine Learning\\PlantVillage_Features\\resnext'
-    run_settings.randomRotate = True
-    run_settings.randomHoriFlip = True
-    run_settings.randomVertFlip = True
+    #run_settings.learning_rate = 1e-5 #default is 1e-3
+    run_settings.identifier = "VGG_Trial2"
+    run_settings.use_cuda = True
+    run_settings.save_weights = True
+    run_settings.num_epochs = 30
+    run_settings.batch_size = 32
+    run_settings.save_freq = 5
 
     train_loader, val_loader, test_loader = getloaders(run_settings)
 
