4d43ff74600092dd2c68bd7b98d84ce1e316d5bd
Brian <cheongb002@gmail.com>
2021-03-29 22:52:04 -0400

    larger FC for models, added test_loader to train_utils

--- utils/loaders.py
+++ utils/loaders.py
@@ -54,7 +54,7 @@ def getloaders(settings):
 
     #Create dataloader
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=settings.batch_size,shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_set,batch_size = 512,shuffle=True) #want largest possible bs for val set for speed
+    val_loader = torch.utils.data.DataLoader(val_set,batch_size = 64,shuffle=True) #want largest possible bs for val set for speed
     test_loader = torch.utils.data.DataLoader(test_set,shuffle=True)
 
     return train_loader, val_loader, test_loader
