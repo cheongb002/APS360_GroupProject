24174e89f31f23a820e2b47f9d84253b3f4f5628
Brian <cheongb002@gmail.com>
2021-03-31 14:26:20 -0400

    Still working to debug bad test performance

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -10,17 +10,23 @@ def train_pretrained():
     # set your settings, customizing from the defaults if necessary, see utils/settings.py for all parameters
     run_settings = settings()
     #run_settings.learning_rate = 1e-5 #default is 1e-3
-    run_settings.identifier = "fix_testacc_1"
+    run_settings.identifier = "densenet_trial1"
     name  = run_settings.identifier
     run_settings.use_cuda = True
     run_settings.save_weights = True
     run_settings.num_epochs = 1
-    run_settings.batch_size = 64
+    run_settings.batch_size = 256
     run_settings.save_freq = 5
 
     train_loader, val_loader, test_loader = getloaders(run_settings)
-    for loader in [val_loader]: #[train_loader,val_loader,test_loader]:
-        for imgs, labels in loader:
-            print(labels)
+    model = create_model("densenet", run_settings) # change as needed (options: vgg, resnet, densenet, googlenet, resnext)
+    #model.eval()
+    if run_settings.use_cuda and torch.cuda.is_available():
+        model.cuda()
+        print("CUDA available")
+    else:
+        print("CUDA not being used")
+    acc = get_accuracy(model,test_loader)
+    print(acc)
 if __name__ == '__main__':
     train_pretrained()
