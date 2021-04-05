21f6e26e5741a7b31edddac450b10b2fa1777c6b
Brian <cheongb002@gmail.com>
2021-04-05 15:36:10 -0400

    Added image paths to inference

--- scripts/inference.py
+++ scripts/inference.py
@@ -7,7 +7,9 @@ from utils.common import create_model, get_model_name
 from PIL import Image
 import os
 
-img_path = "/home/brian/Data/APS360/APS_Project/PlantVillage/Pepper__bell___Bacterial_spot/0a0dbf1f-1131-496f-b337-169ec6693e6f___NREC_B.Spot 9241.JPG"
+#img_path = "/home/brian/Data/APS360/APS_Project/test_images/Potato_healthy.JPG" 
+img_path = "/home/brian/Data/APS360/APS_Project/test_images/Potato_late_blight.JPG" #will be incorrectly classfied
+#img_path = "/home/brian/Data/APS360/APS_Project/test_images/Bell_pepper_bacterial_Spot.JPG"
 
 transformList = []
 
@@ -26,11 +28,15 @@ run_settings.num_epochs = 45
 model = create_model("densenet", run_settings)
 model_path = os.path.join(run_settings.weight_checkpoints, run_settings.identifier, get_model_name(model.name,run_settings,run_settings.num_epochs))
 
+
 if run_settings.use_cuda and torch.cuda.is_available():
     model.cuda()
     print("CUDA available")
 else:
     print("CUDA not being used")
+
+state = torch.load(model_path)
+model.load_state_dict(state)
 
 model.eval()
 
@@ -40,6 +46,7 @@ with Image.open(img_path) as im:
     transformed_im = torch.unsqueeze(transformed_im,0)
     transformed_im = transformed_im.cuda()
     output = model(transformed_im)
+#print(output)
 preds = output.max(1)[1]
 print(int(preds))
 predicted_class = run_settings.classes[int(preds)]
